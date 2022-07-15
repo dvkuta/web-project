38ae29fe16758ee4b144b20067383989eadf4017
David Kůta <dkuta@students.zcu.cz>
2022-07-15 11:24:39 +0200

    dalsi uprava

---
+++ app/Presenters/TruePresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use App\Model\World;
+use Nette;
+use Nette\Application\UI\Form;
+
+
+class TruePresenter extends Nette\Application\UI\Presenter
+{
+
+    public function renderDefault(){
+         $this->template->hello = new Nette\Utils\DateTime();
+    }
+
+}
